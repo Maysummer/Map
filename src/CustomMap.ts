20cff3c3479ff2bd06ad96bc3fddc73107de1b2b
Maysummer <udojyk@gmail.com>
2023-11-07 14:37:24 +0100

    add a marker for generated user on the map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
 //This is created as a replica of google maps but also to ensure the availability of ONLY specific properties we specify
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -10,5 +13,19 @@ export class CustomMap {
         lng: 0
       }
     });
+  }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng
+      }
+    })
+  }
+
+  addCompanyMarker(company: Company) {
+
   }
 }
